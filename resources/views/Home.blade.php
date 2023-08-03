61e9542756e715915b4bda927230d33ea697e94b
rachnavyas <115367369+rachnavyas@users.noreply.github.com>
2023-08-03 12:16:17 +0530

    update

---
+++ resources/views/Home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.main')
+@section('main-section')
+<div style="height:300px;width:50px;">
+
+<h1>Home Page</h1>
+</div>
+@endsection
---
+++ resources/views/about.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.main')
+@section('main-section')
+<div style="height:300px;width:150px;">
+<h1>About Us</h1>
+</div>
+@endsection
